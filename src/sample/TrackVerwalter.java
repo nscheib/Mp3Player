e78101f92b3e92fd257976004a7f076031bc6558
fefuhr <felix.fuhrmann@student.hs-rm.de>
2018-11-12 13:39:38 +0100

    fdsgsdf

---
+++ src/sample/TrackVerwalter.java
@@ -0,0 +1,27 @@
+package sample;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class TrackVerwalter {
+
+    private File f = new File("tracks");
+    private File [] fileArray = f.listFiles();
+    private ArrayList<String> songs = new ArrayList<String>();
+
+    public void songListe(){
+
+        if (fileArray != null) {
+            for (int i = 0; i < fileArray.length; i++) {
+                if (fileArray[i].getName().endsWith(".mp3")){
+                    songs.add(fileArray[i].getName());
+                }
+            }
+        }
+    }
+
+
+    public ArrayList<String> getAllSongs() {
+        return songs;
+    }
+}
